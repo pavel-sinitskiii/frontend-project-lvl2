25790033dd35d327a5d837277f5ecb2a2c286cb1
Pavel Sinitski <pavel.sinitskiii@gmail.com>
2021-07-25 19:59:08 +0300

    Edited index.js

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ const getCompareFiles = (filepath1, filepath2) => {
       return ` ${operators[0]} ${element}: ${file1[element]}`;
     } if (!_.has(file1, element) && _.has(file2, element)) {
       return ` ${operators[1]} ${element}: ${file2[element]}`;
-    } if (_.has(file1, element) && _.has(file2, element) && file1[element] === file2[element]) {
-      return ` ${operators[2]} ${element}: ${file1[element]}`;
+    } if (_.has(file1, element) && _.has(file2, element) && file1[element] !== file2[element]) {
+      return ` ${operators[0]} ${element}: ${file1[element]}\n ${operators[1]} ${element}: ${file2[element]}`;
     }
-    return ` ${operators[0]} ${element}: ${file1[element]}\n ${operators[1]} ${element}: ${file2[element]}`;
+    return ` ${operators[2]} ${element}: ${file1[element]}`;
   });
   return `{\n${resultObj.join('\n')}\n}`;
 };
