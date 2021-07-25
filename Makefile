9fb4420e9ee83256977268b47a5c5a32e687b5ea
Pavel Sinitski <pavel.sinitskiii@gmail.com>
2021-07-25 19:53:53 +0300

    Add Eslint

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 gendiff :
 	node bin/gendiff.js
+
+lint:
+	npx eslint .
--- src/index.js
+++ src/index.js
@@ -11,16 +11,13 @@ const getCompareFiles = (filepath1, filepath2) => {
   const resultObj = allKeys.map((element) => {
     if (_.has(file1, element) && !_.has(file2, element)) {
       return ` ${operators[0]} ${element}: ${file1[element]}`;
-    } else if (!_.has(file1, element) && _.has(file2, element)) {
+    } if (!_.has(file1, element) && _.has(file2, element)) {
       return ` ${operators[1]} ${element}: ${file2[element]}`;
-    } else if (_.has(file1, element) && _.has(file2, element) &&
-      file1[element] === file2[element]) {
+    } if (_.has(file1, element) && _.has(file2, element) && file1[element] === file2[element]) {
       return ` ${operators[2]} ${element}: ${file1[element]}`;
-    } else if (_.has(file1, element) && _.has(file2, element) &&
-      file1[element] !== file2[element]) {
-      return ` ${operators[0]} ${element}: ${file1[element]}\n ${operators[1]} ${element}: ${file2[element]}`;
     }
+    return ` ${operators[0]} ${element}: ${file1[element]}\n ${operators[1]} ${element}: ${file2[element]}`;
   });
-  return  `{\n${ resultObj.join('\n') }\n}`;
+  return `{\n${resultObj.join('\n')}\n}`;
 };
 export default getCompareFiles;
