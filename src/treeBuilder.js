981d000a894cb0d90aca242adf623a5915eff4a2
Pavel Sinitski <pavel.sinitskiii@gmail.com>
2021-11-06 18:00:18 +0300

    Changed treeBuilder.js

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -7,22 +7,14 @@ const buildTree = (data1, data2) => {
     const data1Value = data1[key];
     const data2Value = data2[key];
     if (_.isPlainObject(data1Value) && _.isPlainObject(data2Value)) {
-      return {
-        name: key, type: 'nested', value: buildTree(data1Value, data2Value),
-      };
+      return { name: key, type: 'nested', value: buildTree(data1Value, data2Value) };
     }
     if (!_.has(data2, key)) {
-      return {
-        name: key, type: 'removed', value: data1Value,
-      };
+      return { name: key, type: 'removed', value: data1Value };
     } if (!_.has(data1, key)) {
-      return {
-        name: key, type: 'added', value: data2Value,
-      };
+      return { name: key, type: 'added', value: data2Value };
     } if (_.isEqual(data1Value, data2Value)) {
-      return {
-        name: key, type: 'unchanged', value: data1Value,
-      };
+      return { name: key, type: 'unchanged', value: data1Value };
     }
     return {
       name: key, type: 'updated', value1: data1Value, value2: data2Value,
