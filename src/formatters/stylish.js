b08851bde3e09594b0f715635413866e6b20c100
Pavel Sinitski <pavel.sinitskiii@gmail.com>
2021-11-18 23:57:32 +0300

    Correcting bugs 4.1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 
-const indent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
+const indent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
   const keys = _.keys(data);
-  const line = keys.map((key) => `${indent(depth)}${key}: ${stringify(data[key], depth + 1)}`);
-  return ['{', ...line, `${indent(depth - 1)}}`].join('\n');
+  const line = keys.map((key) => `${indent(depth + 1)}  ${key}: ${stringify(data[key], depth + 1)}`);
+  return ['{', ...line, `${indent(depth)}  }`].join('\n');
 };
 
 const stylish = (innerTree) => {
@@ -16,20 +16,20 @@ const stylish = (innerTree) => {
     const line = tree.map((node) => {
       switch (node.type) {
         case 'nested':
-          return `${indent(depth)}${node.key}: ${format(node.children, depth + 1)}`;
+          return `${indent(depth)}  ${node.key}: ${format(node.children, depth + 1)}`;
         case 'removed':
-          return `${indent(depth - 1)}  - ${node.key}: ${stringify(node.value, depth + 1)}`;
+          return `${indent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
         case 'added':
-          return `${indent(depth - 1)}  + ${node.key}: ${stringify(node.value, depth + 1)}`;
+          return `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
         case 'updated':
-          return `${indent(depth - 1)}  - ${node.key}: ${stringify(node.value1, depth + 1)}\n${indent(depth - 1)}  + ${node.key}: ${stringify(node.value2, depth + 1)}`;
+          return `${indent(depth)}- ${node.key}: ${stringify(node.value1, depth)}\n${indent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
         case 'unchanged':
-          return `${indent(depth)}${node.key}: ${stringify(node.value, depth + 1)}`;
+          return `${indent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
         default:
           throw new Error(`Unknown status: '${node.type}'!`);
       }
     });
-    return ['{', ...line, `${indent(depth - 1)}}`].join('\n');
+    return ['{', ...line, `${indent(depth).slice(2)}}`].join('\n');
   };
   return format(innerTree);
 };
